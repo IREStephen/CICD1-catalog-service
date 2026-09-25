06342df2b5022846598accea22c737216b389f18
StephenDuffy-STUDENT <g00465649@atu.ie>
2026-09-25 20:30:51 +0100

    Made the product as a JPA entity

--- src/main/java/ie/atu/cicd1/catalog/cicd1catalogservice/model/Product.java
+++ src/main/java/ie/atu/cicd1/catalog/cicd1catalogservice/model/Product.java
@@ -1,16 +1,33 @@
 package ie.atu.cicd1.catalog.cicd1catalogservice.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
+import jakarta.persistence.*;
 import java.math.BigDecimal;
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private BigDecimal name;
-    private Double price;
+
+    private String name;
+    private BigDecimal price;
+
+    public Product() {}
+
+    public Product(Long id, String name, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public BigDecimal getPrice() { return price; }
+    public void setPrice(BigDecimal price) { this.price = price; }
 }
